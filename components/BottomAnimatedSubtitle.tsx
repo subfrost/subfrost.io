bab770fefac9c325dbae61a4ef0310eeb726b670
subfrostdev <subzeroresearchltd@gmail.com>
2025-08-04 21:33:53 -0400

    style: decrease subtitle font size

--- components/BottomAnimatedSubtitle.tsx
+++ components/BottomAnimatedSubtitle.tsx
@@ -5,7 +5,7 @@ import type React from "react"
 const BottomAnimatedSubtitle: React.FC = () => {
   return (
     <div className="subtitle-container h-6 md:h-7.5 lg:h-9 flex items-center justify-center">
-      <div className="text-xs sm:text-sm md:text-base lg:text-lg text-[#284372]  font-bold">
+      <div className="text-xs sm:text-xs md:text-sm lg:text-base text-[#284372] font-bold">
         BTC SYNTHETICS ON BITCOIN LAYER 1: frBTC & dxBTC
       </div>
     </div>
