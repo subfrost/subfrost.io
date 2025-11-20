95069d2ebbb0121c1c75a3cae64cc15014469ea0
ignoblehierarch <ignoblehierarch@proton.me>
2025-11-20 14:26:02 -0500

    updates

--- components/YieldFlowChart.tsx
+++ components/YieldFlowChart.tsx
@@ -51,7 +51,7 @@ export default function YieldFlowChart() {
           </filter>
           
           {/* Arrow marker */}
-          <marker id="arrowhead" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="6" markerHeight="6" orient="auto-start-reverse">
+          <marker id="arrowhead" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="6" markerHeight="6" orient="auto">
             <path d="M 0 0 L 10 5 L 0 10 z" fill="#60a5fa" />
           </marker>
         </defs>
@@ -100,17 +100,17 @@ export default function YieldFlowChart() {
         
         {/* Animated flow lines from sources to yvfrBTC */}  
         <g id="flowToYv" opacity="0.8">
-          <path d="M 170 185 Q 220 240, 280 280" stroke="url(#yvGradient)" strokeWidth="3" fill="none" 
+          <path d="M 160 195 Q 220 240, 280 280" stroke="url(#yvGradient)" strokeWidth="3" fill="none" 
                 filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)">
-            <animate attributeName="stroke-dashoffset" values="0;12" dur="1s" repeatCount="indefinite" />
+            <animate attributeName="stroke-dashoffset" values="12;0" dur="1s" repeatCount="indefinite" />
           </path>
           <path d="M 300 195 L 300 280" stroke="url(#yvGradient)" strokeWidth="3" fill="none" 
                 filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)">
-            <animate attributeName="stroke-dashoffset" values="0;12" dur="1s" repeatCount="indefinite" begin="0.3s" />
+            <animate attributeName="stroke-dashoffset" values="12;0" dur="1s" repeatCount="indefinite" begin="0.3s" />
           </path>
-          <path d="M 430 185 Q 380 240, 320 280" stroke="url(#yvGradient)" strokeWidth="3" fill="none" 
+          <path d="M 440 195 Q 380 240, 320 280" stroke="url(#yvGradient)" strokeWidth="3" fill="none" 
                 filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)">
-            <animate attributeName="stroke-dashoffset" values="0;12" dur="1s" repeatCount="indefinite" begin="0.6s" />
+            <animate attributeName="stroke-dashoffset" values="12;0" dur="1s" repeatCount="indefinite" begin="0.6s" />
           </path>
         </g>
         
@@ -122,10 +122,6 @@ export default function YieldFlowChart() {
           </rect>
           <text x="300" y="335" textAnchor="middle" fontSize="24" fontWeight="bold" fill="white">yvfrBTC</text>
           <text x="300" y="360" textAnchor="middle" fontSize="14" fill="#e0e7ff">Yield Aggregator</text>
-          
-          {/* Corner accents */}
-          <rect x="220" y="300" width="20" height="20" fill="none" stroke="#60a5fa" strokeWidth="2" opacity="0.7" />
-          <rect x="360" y="360" width="20" height="20" fill="none" stroke="#60a5fa" strokeWidth="2" opacity="0.7" />
         </g>
         
         {/* Mining Rewards source for ftrBTC */}
@@ -146,31 +142,29 @@ export default function YieldFlowChart() {
           </rect>
           <text x="600" y="375" textAnchor="middle" fontSize="24" fontWeight="bold" fill="white">ftrBTC</text>
           <text x="600" y="400" textAnchor="middle" fontSize="14" fill="#ede9fe">Futures Market</text>
-          
-          {/* Corner accents */}
-          <rect x="520" y="340" width="20" height="20" fill="none" stroke="#a78bfa" strokeWidth="2" opacity="0.7" />
-          <rect x="660" y="400" width="20" height="20" fill="none" stroke="#a78bfa" strokeWidth="2" opacity="0.7" />
         </g>
         
         {/* Flow from Market Action to ftrBTC */}
         <path d="M 450 225 Q 450 280, 550 340" stroke="url(#ftrGradient)" strokeWidth="3" fill="none" 
-              filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)" />
+              filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)">
+          <animate attributeName="stroke-dashoffset" values="12;0" dur="1s" repeatCount="indefinite" />
+        </path>
         
         {/* Flow from mining to ftrBTC */} 
         <path d="M 650 230 Q 720 280, 660 340" stroke="url(#ftrGradient)" strokeWidth="3" fill="none" 
               filter="url(#glow)" strokeDasharray="8,4" markerEnd="url(#arrowhead)">
-          <animate attributeName="stroke-dashoffset" values="0;12" dur="1s" repeatCount="indefinite" />
+          <animate attributeName="stroke-dashoffset" values="12;0" dur="1s" repeatCount="indefinite" />
         </path>
         
         {/* Flows from yvfrBTC and ftrBTC to dxBTC */}
         <g id="flowToDx" opacity="0.8">
           <path d="M 340 380 Q 380 450, 440 490" stroke="url(#dxGradient)" strokeWidth="4" fill="none" 
                 filter="url(#strongGlow)" strokeDasharray="10,5" markerEnd="url(#arrowhead)">
-            <animate attributeName="stroke-dashoffset" values="0;15" dur="1.5s" repeatCount="indefinite" />
+            <animate attributeName="stroke-dashoffset" values="15;0" dur="1.5s" repeatCount="indefinite" />
           </path>
           <path d="M 560 420 Q 520 450, 480 490" stroke="url(#dxGradient)" strokeWidth="4" fill="none" 
                 filter="url(#strongGlow)" strokeDasharray="10,5" markerEnd="url(#arrowhead)">
-            <animate attributeName="stroke-dashoffset" values="0;15" dur="1.5s" repeatCount="indefinite" begin="0.5s" />
+            <animate attributeName="stroke-dashoffset" values="15;0" dur="1.5s" repeatCount="indefinite" begin="0.5s" />
           </path>
         </g>
         
@@ -184,9 +178,6 @@ export default function YieldFlowChart() {
           <text x="460" y="540" textAnchor="middle" fontSize="32" fontWeight="bold" fill="white">dxBTC</text>
           <text x="460" y="568" textAnchor="middle" fontSize="16" fill="#d1fae5">One-Click Yield</text>
           
-          {/* Corner accents - more prominent for final destination */}
-          <rect x="360" y="500" width="25" height="25" fill="none" stroke="#34d399" strokeWidth="3" opacity="0.8" />
-          <rect x="535" y="565" width="25" height="25" fill="none" stroke="#34d399" strokeWidth="3" opacity="0.8" />
           
           {/* Pulsing glow effect */}
           <rect x="360" y="500" width="200" height="90" rx="15" fill="none" 
